800c8eba7cce21f9a3b926ca3fe7e5cdd99e15cf
definho <fixilon@yahoo.com>
2018-03-26 22:34:33 +0200

    reindent

--- friis.php
+++ friis.php
@@ -1,9 +1,9 @@
 <?php 
 
-$dataPoints = array();
+    $dataPoints = array();
 
-$gt = $_POST['n1'];
-$gr = $_POST['n2'];
+    $gt = $_POST['n1'];
+    $gr = $_POST['n2'];
     $d = $_POST['n3']; //udaljenost
     $pt = $_POST['n4'];
     $freq = $_POST['n5'];
